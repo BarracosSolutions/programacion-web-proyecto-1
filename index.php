42a3cd53c91dcdae39b3f08b168bd4e91aee7d0e
Omar Segura <omarseguvi@gmail.com>
2018-03-10 22:34:48 -0600

    index html structure base

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <header>
+            <h1>PHP File Management</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Share</a></li>
+                    <li><a href="#">About</a></li>
+                </ul>
+            </nav>
+        <header>
+        <main>
+        </main>
+        <footer></footer>
+    </body>
+</html>
+
